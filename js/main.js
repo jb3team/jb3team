94ce8bc0c6c148c7d52bf5b1e5068f3b406b6631
ryusunwoo <72438634+ryusunwoo@users.noreply.github.com>
2020-11-16 14:15:33 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -50,87 +50,87 @@ $(function(){
 
   // 이미지 속성 변경: attr(속성명, 값) 1번째 슬라이드
   $('img').eq(8).mouseover(function(){
-    $('img').eq(8).attr('src', '../images/fund_product_hover01.png');
+    $('img').eq(8).attr('src', './images/fund_product_hover01.png');
   })
   $('img').mouseout(function(){
-    $('img').eq(8).attr('src', '../images/inv_box01.png');
+    $('img').eq(8).attr('src', './images/inv_box01.png');
   })
 
   $('img').eq(9).mouseover(function(){
-    $('img').eq(9).attr('src', '../images/fund_product_hover02.png');
+    $('img').eq(9).attr('src', './images/fund_product_hover02.png');
   })
   $('img').mouseout(function(){
-    $('img').eq(9).attr('src', '../images/inv_box02.png');
+    $('img').eq(9).attr('src', './images/inv_box02.png');
   })
 
   $('img').eq(10).mouseover(function(){
-    $('img').eq(10).attr('src', '../images/fund_product_hover03.png');
+    $('img').eq(10).attr('src', './images/fund_product_hover03.png');
   })
   $('img').mouseout(function(){
-    $('img').eq(10).attr('src', '../images/inv_box03.png');
+    $('img').eq(10).attr('src', './images/inv_box03.png');
   })
 
   $('img').eq(11).mouseover(function(){
     $('img').eq(11).attr('src', './images/fund_product_hover04.png');
   })
   $('img').mouseout(function(){
-    $('img').eq(11).attr('src', '../images/inv_box04.png');
+    $('img').eq(11).attr('src', './images/inv_box04.png');
   })
   // 이미지 속성 변경: attr(속성명, 값) 2번째 슬라이드
   $('img').eq(12).mouseover(function(){
-    $('img').eq(12).attr('src', '../images/fund_product_hover01.png');
+    $('img').eq(12).attr('src', './images/fund_product_hover01.png');
   })
   $('img').mouseout(function(){
-    $('img').eq(12).attr('src', '../images/inv_box01.png');
+    $('img').eq(12).attr('src', './images/inv_box01.png');
   })
 
   $('img').eq(13).mouseover(function(){
-    $('img').eq(13).attr('src', '../images/fund_product_hover02.png');
+    $('img').eq(13).attr('src', './images/fund_product_hover02.png');
   })
   $('img').mouseout(function(){
-    $('img').eq(13).attr('src', '../images/inv_box02.png');
+    $('img').eq(13).attr('src', './images/inv_box02.png');
   })
 
   $('img').eq(14).mouseover(function(){
-    $('img').eq(14).attr('src', '../images/fund_product_hover03.png');
+    $('img').eq(14).attr('src', './images/fund_product_hover03.png');
   })
   $('img').mouseout(function(){
-    $('img').eq(14).attr('src', '../images/inv_box03.png');
+    $('img').eq(14).attr('src', './images/inv_box03.png');
   })
 
   $('img').eq(15).mouseover(function(){
-    $('img').eq(15).attr('src', '../images/fund_product_hover04.png');
+    $('img').eq(15).attr('src', './images/fund_product_hover04.png');
   })
   $('img').mouseout(function(){
-    $('img').eq(15).attr('src', '../images/inv_box04.png');
+    $('img').eq(15).attr('src', './images/inv_box04.png');
   })
   // 이미지 속성 변경: attr(속성명, 값) 3번째 슬라이드
   $('img').eq(16).mouseover(function(){
-    $('img').eq(16).attr('src', '../images/fund_product_hover01.png');
+    $('img').eq(16).attr('src', './images/fund_product_hover01.png');
   })
   $('img').mouseout(function(){
-    $('img').eq(16).attr('src', '../images/inv_box01.png');
+    $('img').eq(16).attr('src', './images/inv_box01.png');
   })
 
   $('img').eq(17).mouseover(function(){
-    $('img').eq(17).attr('src', '../images/fund_product_hover02.png');
+    $('img').eq(17).attr('src', './images/fund_product_hover02.png');
   })
   $('img').mouseout(function(){
-    $('img').eq(17).attr('src', '../images/inv_box02.png');
+    $('img').eq(17).attr('src', './images/inv_box02.png');
   })
 
   $('img').eq(18).mouseover(function(){
-    $('img').eq(18).attr('src', '../images/fund_product_hover03.png');
+    $('img').eq(18).attr('src', './images/fund_product_hover03.png');
   })
   $('img').mouseout(function(){
-    $('img').eq(18).attr('src', '../images/inv_box03.png');
+    $('img').eq(18).attr('src', './images/inv_box03.png');
   })
 
   $('img').eq(19).mouseover(function(){
-    $('img').eq(19).attr('src', '../images/fund_product_hover04.png');
+    $('img').eq(19).attr('src', './images/fund_product_hover04.png');
   })
   $('img').mouseout(function(){
-    $('img').eq(19).attr('src', '../images/inv_box04.png');
+    $('img').eq(19).attr('src', './images/inv_box04.png');
   })
 
 
